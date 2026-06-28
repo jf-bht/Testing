5b6676aa4f1c3f19ff557e0ff34ddb0b2f2b647a
jafi6404 <jafi6404@bht-berlin.de>
2026-06-28 16:06:21 +0200

    A2: refactor - boilerplate code in den tests durch beforeeach konstruktor ersetzt

--- src/test/java/GroceryCartTest.java
+++ src/test/java/GroceryCartTest.java
@@ -1,10 +1,17 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 class GroceryCartTest {
 
+    private GroceryCart cart;
+    @BeforeEach
+    void setUp() {
+        cart = new GroceryCart();
+    }
+
     @Test
     void testAddItemIncreasesSize() {
-        GroceryCart cart = new GroceryCart();
         GroceryItem item = new GroceryItem("Tomaten", 1.99, 2);
         cart.addItem(item);
         assertEquals(1, cart.getItems().size());
@@ -12,7 +19,6 @@ class GroceryCartTest {
 
     @Test
     void testGetTotalPrice() {
-        GroceryCart cart = new GroceryCart();
         cart.addItem(new GroceryItem("Tomaten", 1.99, 2));
         cart.addItem(new GroceryItem("Milch", 0.99, 1));
         assertEquals(4.97, cart.getTotalPrice(), 0.01);
