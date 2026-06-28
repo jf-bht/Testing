2164417428440cb0bb9dca2f128d6b4d932de4c5
jafi6404 <jafi6404@bht-berlin.de>
2026-06-28 15:55:01 +0200

    A2: refactor - GroceryItem zu record geändert, weniger boilerplate code

--- src/main/java/GroceryItem.java
+++ src/main/java/GroceryItem.java
@@ -1,11 +1,2 @@
-public class GroceryItem {
-    private final String name;
-    private final double price;
-    private final int quantity;
-
-    public GroceryItem(String name, double price, int quantity) {
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
-    }
+public record GroceryItem(String name, double price, int quantity) {
 }
